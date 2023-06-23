b22134ebf78d1283b8b41c293219ee65bed6ec5b
rocksiza <ununivirse1@gmail.com>
2023-06-23 15:40:53 -0400

    Update randomscript.js

--- randomscript.js
+++ randomscript.js
@@ -1,5 +1,5 @@
 window.addEventListener('DOMContentLoaded', function() {
-  var backgrounds = ['background1.jpg', 'background2.jpg', 'background3.jpg', 'background4.jpg']; // Add the file names of your background images
+  var backgrounds = ['background5.jpg', 'background6.jpg']; // Add the file names of your background images
   var randomIndex = Math.floor(Math.random() * backgrounds.length);
   var randomImage = backgrounds[randomIndex];
   document.body.style.backgroundImage = 'url(' + randomImage + ')';
